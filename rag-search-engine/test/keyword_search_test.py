658a42a22a3a15ffab1fd29b2a1cc19777a86310
Roger Pease <rogerpease@gmail.com>
2026-01-25 13:57:32 -0600

    Added Hybrid Search

--- rag-search-engine/test/keyword_search_test.py
+++ rag-search-engine/test/keyword_search_test.py
@@ -10,22 +10,24 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", "cli", "lib"))
 from keyword_search import InvertedIndex
 
 class KeywordSearchTest (unittest.TestCase):
+      
+    def setUp(self):
+        print("Running setup") 
+        moviesfile = os.path.join(os.path.dirname(__file__), "..", "data", "movies.json")
+        with open(moviesfile,'r') as fp:
+           self.movies = json.load(fp)
+
+        self.ivi = InvertedIndex(self.movies)
+        self.ivi.build(self.movies)
 
     def test_keyword_search_test(self):
-        moviesfile = os.path.join(os.path.dirname(__file__), "..", "data", "movies.json")
 
-        with open(moviesfile, 'r') as fp:
-            movies = json.load(fp)["movies"]
-        print(movies[0]) 
-
-        ivi = InvertedIndex(movies)
-        ivi.load()
-        results = ivi.bm25_search("Anbuselvan",10)
+        results = self.ivi.bm25_search("Anbuselvan",10)
 
         for item in results:
             index, score = item
             print ("Index",index,"score",score)
-            print (movies[int(index)]['title'], score)
+            print (self.movies[int(index)]['title'], score)
 
 
 if __name__ == '__main__':
